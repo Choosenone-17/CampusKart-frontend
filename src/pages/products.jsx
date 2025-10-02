f3d2a92f1006130ea2c4d1237ba8d1670e4e5375
Yashvardhan Patel <yashvardhan.patel007@gmail.com>
2025-10-02 09:11:14 +0530

    UI fixes

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -141,7 +141,7 @@ export function Products({ onAddProductClick, onContactSeller }) {
 
         {/* Results */}
         {isLoading ? (
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
             {[...Array(12)].map((_, i) => (
               <div
                 key={i}
